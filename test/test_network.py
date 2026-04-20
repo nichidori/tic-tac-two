86916d73eff8b9af34c12352370888337204592e
Arrafly Dori <arraflydori@gmail.com>
2026-04-20 22:59:26 +0700

    fix: update obsolete test

--- test/test_network.py
+++ test/test_network.py
@@ -1,4 +1,4 @@
-from src.network import PayloadType, encode_payload, decode_payload
+from src.network import *
 
 
 def test_payload_type_values():
@@ -10,42 +10,43 @@ def test_payload_type_values():
 def test_encode_decode_set_player():
     type = PayloadType.SET_PLAYER
     player_1 = True
-    encoded = encode_payload(type, player_1)
-    decoded_type, decoded_data = decode_payload(type, encoded[3:])
-    assert decoded_type == PayloadType.SET_PLAYER
-    assert decoded_data == player_1
+    encoded = encode_payload(SetPlayerPayload(player_1))
+    decoded_payload = decode_payload(type, encoded[3:])
+    assert decoded_payload.type == type
+    assert decoded_payload.player_1 == player_1
 
 
 def test_encode_decode_set_player_false():
     type = PayloadType.SET_PLAYER
     player_1 = False
-    encoded = encode_payload(type, player_1)
-    decoded_type, decoded_data = decode_payload(type, encoded[3:])
-    assert decoded_type == PayloadType.SET_PLAYER
-    assert decoded_data == player_1
+    encoded = encode_payload(SetPlayerPayload(player_1))
+    decoded_payload = decode_payload(type, encoded[3:])
+    assert decoded_payload.type == type
+    assert decoded_payload.player_1 == player_1
 
 
 def test_encode_decode_mark():
     type = PayloadType.MARK
     row, col = 1, 2
-    encoded = encode_payload(type, row, col)
-    decoded_type, decoded_data = decode_payload(type, encoded[3:])
-    assert decoded_type == PayloadType.MARK
-    assert decoded_data == (row, col)
+    encoded = encode_payload(MarkPayload(row, col))
+    decoded_payload = decode_payload(type, encoded[3:])
+    assert decoded_payload.type == type
+    assert decoded_payload.row == row
+    assert decoded_payload.col == col
 
 
 def test_encode_decode_mark_zero():
     type = PayloadType.MARK
     row, col = 0, 0
-    encoded = encode_payload(type, row, col)
-    decoded_type, decoded_data = decode_payload(type, encoded[3:])
-    assert decoded_type == PayloadType.MARK
-    assert decoded_data == (row, col)
+    encoded = encode_payload(MarkPayload(row, col))
+    decoded_payload = decode_payload(type, encoded[3:])
+    assert decoded_payload.type == type
+    assert decoded_payload.row == row
+    assert decoded_payload.col == col
 
 
 def test_encode_decode_exit():
     type = PayloadType.EXIT
-    encoded = encode_payload(type)
-    decoded_type, decoded_data = decode_payload(type, encoded[3:])
-    assert decoded_type == PayloadType.EXIT
-    assert decoded_data is None
+    encoded = encode_payload(ExitPayload())
+    decoded_payload = decode_payload(type, encoded[3:])
+    assert decoded_payload.type == type
